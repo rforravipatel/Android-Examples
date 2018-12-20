d2fc678985aaa5189fe385efdb4e184773fa6b99
Ravi Patel <ravipatel@dyn-169-226-245-107.wireless.albany.edu>
2018-12-20 15:34:44 -0500

    ButtonTextFieldDemo

---
+++ ButtonTextFieldDemo/app/src/main/java/com/example/android/buttontextfielddemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.buttontextfielddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void clickFunction(View view){
+
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Log.i("info", myTextField.getText().toString());
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
